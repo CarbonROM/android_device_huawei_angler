af28ae45a6e01af198dc0a0a6754d91fe4d141b5
Patrick Campanale <patrickappcreator@gmail.com>
2016-02-20 10:08:40 -0700

    Carbon bringup
    Change-Id: I2a687b2978eb6b229daad58964098f599dd1fbae

---
+++ BoardConfigCarbon.mk
@@ -0,0 +1,20 @@
+# Inline kernel
+#BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+#TARGET_KERNEL_SOURCE := kernel/huawei/angler
+#TARGET_KERNEL_CONFIG := angler_defconfig
+#TARGET_KERNEL_ARCH := arm64
+#TARGET_KERNEL_HEADER_ARCH := arm64
+#TARGER_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+#TARGET_USES_UNCOMPRESSED_KERNEL := true
+
+# Use device's audio_effects.conf
+TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF := true
+
+# Ignore vendor audio_effects.conf
+TARGET_IGNORE_VENDOR_AUDIO_EFFECTS_CONF := true
+
+# Enable vendor image symlink
+BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
+
+# Keymaster - Wait for qseecom to load
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
---
+++ carbon_angler.mk
@@ -0,0 +1,35 @@
+# Inherit carbon-specific board config
+include device/huawei/angler/BoardConfigCarbon.mk
+
+# Inherit base AOSP device configuration
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+# Inherit APNs list
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/carbon/config/common.mk)
+
+# Bootanimation
+TARGET_BOOTANIMATION_SIZE := 960x640
+
+# Allow tethering without provisioning app
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.tethering.noprovisioning=true
+
+# SELinux
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
+# Override product naming for Carbon
+PRODUCT_NAME := carbon_angler
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := HUAWEI
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT="google/angler/angler:6.0.1/MMB29Q/2480792:user/release-keys" \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29Q 2480792 release-keys" \
+    BUILD_ID=MMB29Q
