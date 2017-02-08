4bc326cc4da76314a7fc2e4a564e7db8cb4814d0
Adin Kwok <adin.kwok@carbonrom.org>
2017-02-07 21:35:51 -0800

    angler: add device maintainer info
    Change-Id: I7dd17c0f18d73cfe4dae9ad098d8b306fb157948

--- carbon_angler.mk
+++ carbon_angler.mk
@@ -39,3 +39,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="google/angler/angler:7.1.1/N4F26J/3549317:user/release-keys" \
     PRIVATE_BUILD_DESC="angler-user 7.1.1 N4F26J 3549317 release-keys"
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.carbon.maintainer="kwoktopus and frap129"
