536488bdfb5b98a8475d54c3bd82040e6298bd6c
Josh Chasky <jchasky@gmail.com>
2017-06-09 06:25:54 +0530

    Angler: Update build fingerprint to N2G47W
    Change-Id: Ia29a34ad45b34d2182dc689df6e5437307a97e31
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53.a57
 
+SDCLANG := true
+TARGET_USE_SDCLANG := true
+
 ENABLE_CPUSETS := true
 
 # Inline kernel building
--- carbon_angler.mk
+++ carbon_angler.mk
@@ -33,8 +33,8 @@ PRODUCT_MODEL := Nexus 6P
 TARGET_VENDOR := huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="google/angler/angler:7.1.2/N2G47O/3852959:user/release-keys" \
-    PRIVATE_BUILD_DESC="angler-user 7.1.2 N2G47O 3852959 release-keys"
+    BUILD_FINGERPRINT="google/angler/angler:7.1.2/N2G47W/3938523:user/release-keys" \
+    PRIVATE_BUILD_DESC="angler-user 7.1.2 N2G47W 3938523 release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.carbon.maintainer="kwoktopus/frap129"
